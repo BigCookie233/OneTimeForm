b893cbc9d412f8fe761b0f5c427a5e80abfe503e
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-04-06 22:18:15 +0800

    Initial commit

---
+++ src/main/java/io/github/bigcookie233/onetimeform/OneTimeFormApplication.java
@@ -0,0 +1,13 @@
+package io.github.bigcookie233.onetimeform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimeFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTimeFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bigcookie233/onetimeform/OneTimeFormApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bigcookie233.onetimeform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTimeFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
